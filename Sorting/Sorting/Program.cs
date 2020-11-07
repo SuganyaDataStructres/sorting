3eb5cdc4eaab33a41740d488361aa54f96b6d6f4
Venkatesan Panneerselvam <vselvam@vmware.com>
2020-11-07 13:53:48 +0530

    i wrote sorting names

---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selection sort\nInsertion sort\nQuick Sort\nShell Sort\nMerge Sort\nBubble Sort");
+        }
+    }
+}
